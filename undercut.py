76399fd40e53f9c0e3b5caf39aa59a3f6b166b3a
Jack Chan <46509894+jackchan0528@users.noreply.github.com>
2020-12-23 01:44:34 +0800

    3 numeric input accepted
    3 numeric input accepted and generate correct prediction result

--- undercut.py
+++ undercut.py
@@ -1,5 +1,7 @@
+from numpy.core.fromnumeric import transpose
 import pandas as pd
 import numpy as np
+pd.options.mode.chained_assignment = None  # default='warn'
 
 def undercut_excel_unboxer(file_location):
     undercut = pd.read_excel(
@@ -7,20 +9,60 @@ def undercut_excel_unboxer(file_location):
         dtype={"No.": str, "laps_undercut_overcut": str, "delta_position": str},
         index_col = 0
     )
-    undercut = undercut[["laps_undercut_overcut", "delta_position"]]
 
-    for i in undercut.index:
+    weather = pd.read_excel(
+        file_location, sheet_name="weather",
+        dtype={"Skycondition": str, "Precipation type": str, "Temperature": str, "Humidity": str, "Wind speed": str, "Wind bearing": str},
+        index_col = 0
+    ).T
+
+    temperature = weather[["Temperature"]]
+    temperature["Temperature"] = temperature["Temperature"].str[:-2]
+    temperature["Temperature"] = temperature["Temperature"].astype(float)
+    # temperature["Temperature"] = temperature["Temperature"].str[0]
+    
+    humidity = weather[["Humidity"]]
+    humidity["Humidity"] = humidity["Humidity"].astype(float)
+
+    # print(temperature["Temperature"].values)
+    # print(humidity["Humidity"].values)
+    
+
+    combined_table = undercut[["laps_undercut_overcut", "delta_position"]]
+    combined_table["Temperature"] = ""
+    combined_table["Humidity"] = ""
+    for i in combined_table.index:
+        combined_table["Temperature"][i] = float(temperature.values)
+        combined_table["Humidity"][i] = float(humidity.values)
+
+    # reorder the columns
+    combined_table = combined_table[["laps_undercut_overcut", "Temperature", "Humidity", "delta_position"]]
+
+    for i in combined_table.index:
         # to drop NaN records
-        if (pd.isna(undercut["laps_undercut_overcut"][i]) or pd.isna(undercut["delta_position"][i]) ):
-            undercut = undercut.drop([i])
+        if (pd.isna(combined_table["laps_undercut_overcut"][i]) or pd.isna(combined_table["delta_position"][i]) ):
+            combined_table = combined_table.drop([i])
+
+
+    # print(combined_table)
 
     # change data from pandas object to numpy object
-    undercut_numpy = undercut.to_numpy()
+    combined_table_numpy = combined_table.to_numpy()
     # change data type from string to integer
-    undercut_numpy = undercut_numpy.astype(int)
+    combined_table_numpy = combined_table_numpy.astype(float)
 
-    undercut_numpy_lip, undercut_numpy_pc = np.hsplit(undercut_numpy, 2)
-    return undercut_numpy_lip, undercut_numpy_pc
+    # print(combined_table_numpy)
+    # print(np.shape(combined_table_numpy)[1])
+
+    # np.array_split(combined_table_numpy_lip, split_idx, axis=0)
+
+    # combined_table_numpy_lip, combined_table_numpy_pc = np.array_split(combined_table_numpy, 3, axis=1)
+    combined_table_numpy_lip = combined_table_numpy[:,:3].astype(float)
+    combined_table_numpy_pc = combined_table_numpy[:,-1].astype(int)
+
+    # print(combined_table_numpy_lip)
+    # print(combined_table_numpy_pc)
+    return combined_table_numpy_lip, combined_table_numpy_pc
 
 # calling all data2019.xlsx files
 file_names = ["abudhabi2019", "austrian2019", "azerbaijan2019", "bahrain2019", "belgian2019", 
@@ -33,6 +75,16 @@ for i in file_names:
     tp_undercut_numpy_lip, tp_undercut_numpy_pc = undercut_excel_unboxer("./data2019/"+i+".xlsx")
     undercut_numpy_lip = np.concatenate((undercut_numpy_lip, tp_undercut_numpy_lip), axis = 0)
     undercut_numpy_pc = np.concatenate((undercut_numpy_pc, tp_undercut_numpy_pc), axis = 0)
+
+print(undercut_numpy_lip)
+print(undercut_numpy_pc)
+
+# # concatenate other factors
+# undercut_numpy_lip = undercut_numpy_lip.tolist()
+# for i in undercut_numpy_lip:
+#     i.append(300)
+# undercut_numpy_lip = np.array(undercut_numpy_lip)
+# print(undercut_numpy_lip)
 # print(undercut_numpy_lip, undercut_numpy_pc)
 
 # apply tensorflow logics
@@ -44,13 +96,14 @@ logger.setLevel(logging.ERROR)
 lap_in_pit    = undercut_numpy_lip
 position_change = undercut_numpy_pc
 
-l0 = tf.keras.layers.Dense(units=4, input_shape=[1])
+# input shape means how many input is taken
+l0 = tf.keras.layers.Dense(units=4, input_shape=[3])
 l1 = tf.keras.layers.Dense(units=4)
 l2 = tf.keras.layers.Dense(units=1)
 
 model = tf.keras.Sequential([l0, l1, l2])
 
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
-model.fit(lap_in_pit, position_change, epochs=500, verbose=False)
+history = model.fit(lap_in_pit, position_change, epochs=500, verbose=False)
 print("Finished training the model")
-print("Model predicts that 25 th lap in pit will result in: {} position change".format(model.predict([25])))
+print("Model predicts that 25 th lap in pit will result in: {} position change".format(model.predict([[25,79,0.55]])))
